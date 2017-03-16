7cd7177cef00125f0c8ad02e5bb2b3ec008eeaef
francescobianco <info.francescobianco@gmail.com>
2017-03-16 19:21:23 +0100

    version 0.0.3

--- src/watch-log.js
+++ src/watch-log.js
@@ -1,5 +1,19 @@
 
-//
-var path = process.cwd();
+var fs = require('fs');
 
-console.log(path);
+var base = process.cwd();
+var file = base + "/watch.log.js";
+
+module.exports = {
+    run: function(args) {
+        fs.stat(file, function(err, stat) {
+            if(err == null) {
+                require(file);
+            } else if(err.code == 'ENOENT') {
+                console.log("watch-log: config file not found.");
+            }
+        });
+    }
+}
+
+
